eafe4ceb63465ff402d1f9f8e1d9344f4adb6968
NahrulK <khayattullah73@gmail.com>
2020-10-24 12:48:56 +0800

    wkwk solved, atara rotate Y dan X seharusnya gak ada tanda koma

--- 3dCardEffect/app.js
+++ 3dCardEffect/app.js
@@ -5,13 +5,11 @@ const container = document.querySelector(".container");
 
 // Now moving the animation
 
-container.addEventListener("mousemove",(e)=> {
+container.addEventListener("mousemove",function(e) {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
     let yAxis = (window.innerHeight / 2 - e.pageX) / 10;
 
-    console.log(xAxis,yAxis);
-
-    // card.style.transform = 'rotateY('+yAxis+'deg), rotateX('+xAxis+'deg)';
-    card.style.color = "black";
-
+    card.style.transform = 'rotateY('+yAxis+'deg) rotateX('+xAxis+'deg)';
+    
 });
+
